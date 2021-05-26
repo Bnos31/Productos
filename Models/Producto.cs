26131b39e62aedf8a6fae78ac0408f1dbf051ab4
Bryan Ojeda <limpsket.bra@gmail.com>
2021-05-25 19:15:03 -0500

    ultimos cambios

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -5,5 +5,11 @@ namespace Productos.Models
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Foto { get; set; }
+
+        public string Descripción { get; set; }
+        public string Precio { get; set; }
+        public int Celular { get; set; }
+        public string Lugar { get; set; }
+        public string Categoria { get; set; }
     }
 }
